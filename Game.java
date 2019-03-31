b8a6ae9378bec12774bf57c7e99254977c7feed4
Unknown <raulfmansur@hotmail.com>
2019-03-30 23:17:30 -0300

    Arcade 201 - 1:N relationship with category

--- Game.java
+++ Game.java
@@ -5,15 +5,17 @@ import java.util.ArrayList;
 public class Game implements FileObject {
 	protected int id;
 	protected String name;
+	protected int categoryID;
 	protected String descr;
 	protected String company;
 	protected float price;
 	protected float size;
 	protected short year;
 
-    public Game(String name, String descr, String company, float price, float size, short year) {
+    public Game(String name, int categoryID, String descr, String company, float price, float size, short year) {
         this.id = -1;
         this.name = name;
+        this.categoryID = categoryID;
         this.descr = descr;
         this.company = company;
         this.price = price;
@@ -24,6 +26,7 @@ public class Game implements FileObject {
     public Game() {
         this.id = -1;
         this.name = "";
+        this.categoryID = -1;
         this.descr = "";
         this.company = "";
         this.price = 0.0f;
@@ -44,6 +47,7 @@ public class Game implements FileObject {
         DataOutputStream out = new DataOutputStream(data);
         out.writeInt(this.id);
         out.writeUTF(this.name);
+        out.writeInt(this.categoryID);
         out.writeUTF(this.descr);
         out.writeUTF(this.company);
         out.writeFloat(this.price);
@@ -57,15 +61,17 @@ public class Game implements FileObject {
         DataInputStream in = new DataInputStream(data);
         this.id = in.readInt();
         this.name = in.readUTF();
+        this.categoryID = in.readInt();
         this.descr = in.readUTF();
         this.company = in.readUTF();
         this.price = in.readFloat();
         this.size = in.readFloat();
         this.year = in.readShort();
     }
-    public String toString(){
+    public String toString(String categoryName){
         return "Id: " + this.id +
                "\nNome: " + this.name +
+               "\nCategoria: " + categoryName +
                "\nDescrição: " + this.descr +
                "\nEmpresa: " + this.company +
                "\nPreço: R$" + this.price +
--- Main.java
+++ Main.java
@@ -13,7 +13,11 @@ public class Main {
 				Archive<Game> gameFile;
 
 				gameFile = new Archive<>(Game.class.getConstructor(), "./db/game.db", "./db/index.db");
-				gameFile.include(UserInterface.readGame(), -1);
+				Game game = UserInterface.readGame();
+				if(game != null)
+					gameFile.include(game, -1);
+				else
+					System.out.println("\n-> ID Categoria Não Encontrado!\n");	
 
 				break;
 			}
@@ -32,15 +36,24 @@ public class Main {
 				break;
 			}
 
-			// EDITAR DADOS
+			// EDITAR DADOS DE JOGO
 			case 3: {
 				Archive<Game> gameFile;
 				int id = UserInterface.readID();
 
 				gameFile = new Archive<>(Game.class.getConstructor(), "./db/game.db", "./db/index.db");
-				if (gameFile.exclude(id)){
-					gameFile.include(UserInterface.readGame(), id);
-					System.out.println("\n-> Jogo Alterado com Sucesso!\n");				
+
+				// verifica se jogo existe
+				if(gameFile.read(id) != null){
+					Game game = UserInterface.readGame();
+					// se categoria é valida
+					if(game != null){
+						gameFile.exclude(id);
+						gameFile.include(game, id);
+						System.out.println("\n-> Jogo Alterado com Sucesso!\n");				
+					}
+					else
+						System.out.println("\n-> ID CATEGORIA Não Encontrado!\n");	
 				}
 				else
 					System.out.println("\n-> ID Não Encontrado!\n");	
@@ -51,13 +64,19 @@ public class Main {
 			// BUSCAR JOGO
 			case 4: {
 				Archive<Game> gameFile;
+				Archive<Category> categoryFile;
 				int id = UserInterface.readID();
 
 				gameFile = new Archive<>(Game.class.getConstructor(), "./db/game.db", "./db/index.db");
+				categoryFile = new Archive<>(Category.class.getConstructor(), "./db/category.db", "./db/index_category.db");
+				
+				Category category = categoryFile.read(id);
 				Game game = gameFile.read(id);
+
 				if ( game != null ){
 					System.out.println("\n-> Jogo Encontrado!\n");
-					System.out.println(game.toString());
+
+					System.out.println(game.toString(category.name));
 				}
 				else
 					System.out.println("\n-> ID Não Encontrado!\n");
@@ -68,19 +87,126 @@ public class Main {
 			// LISTAR JOGOS
 			case 5: {
 				Archive<Game> gameFile;
-
-				gameFile = new Archive<>(Game.class.getConstructor(), "./db/game.db", "./db/index.db");
+				Archive<Category> categoryFile;
 				
+				gameFile = new Archive<>(Game.class.getConstructor(), "./db/game.db", "./db/index.db");
+				categoryFile = new Archive<>(Category.class.getConstructor(), "./db/category.db", "./db/index_category.db");
+
 				System.out.println("");
-				gameFile.list().forEach((game) -> 
-					System.out.println(game.toString())
-				);
+				gameFile.list().forEach((game) -> {
+					try {
+				    	Category category = categoryFile.read(game.categoryID);
+						System.out.println(game.toString(category.name));
+				    } 
+				    catch(Exception e) {
+						e.printStackTrace();
+					}
+				});
 
 				break;
 			}
 
-			// MOSTRAR METADADOS DO DB
+			// ADICIONAR NOVA CATEGORIA
 			case 6: {
+				Archive<Category> categoryFile;
+
+				categoryFile = new Archive<>(Category.class.getConstructor(), "./db/category.db", "./db/index_category.db");
+				categoryFile.include(UserInterface.readCategory(), -1);
+
+				break;
+			}
+
+			// REMOVER CATEGORIA
+			case 7: {	
+				Archive<Game> gameFile;
+				Archive<Category> categoryFile;
+				int id = UserInterface.readID();
+
+				gameFile = new Archive<>(Game.class.getConstructor(), "./db/game.db", "./db/index.db");
+				categoryFile = new Archive<>(Category.class.getConstructor(), "./db/category.db", "./db/index_category.db");
+
+				Category category = categoryFile.read(id);
+				int cont = 0;
+				if( category != null) {
+					for(Game game: gameFile.list()){
+						if(game.categoryID == id){
+							cont++;
+						}
+					}
+					if(cont>0)
+						System.out.println("Não foi possível remover.\nHá " + cont + "jogos atribuídos a essa categoria");
+					else{
+						categoryFile.exclude(id);
+						System.out.println("\n-> Categoria Removida!\n");
+					}
+				}
+				else{
+					System.out.println("\n-> Categoria Não Encontrada!\n");
+				}
+
+				fim:
+
+				break;
+			}
+
+			// EDITAR CATEGORIA
+			case 8: {
+				Archive<Category> categoryFile;
+				int id = UserInterface.readID();
+
+				categoryFile = new Archive<>(Category.class.getConstructor(), "./db/category.db", "./db/index_category.db");
+				if (categoryFile.exclude(id)){
+					categoryFile.include(UserInterface.readCategory(), id);
+					System.out.println("\n-> Categoria Alterada com Sucesso!\n");				
+				}
+				else
+					System.out.println("\n-> ID Não Encontrado!\n");	
+
+				break;
+			}
+
+			// BUSCAR CATEGORIA
+			case 9: {
+				Archive<Category> categoryFile;
+				int id = UserInterface.readID();
+
+				categoryFile = new Archive<>(Category.class.getConstructor(), "./db/category.db", "./db/index_category.db");
+				Category category = categoryFile.read(id);
+				if ( category != null ){
+					System.out.println("\n-> Categoria Encontrada!\n");
+					System.out.println(category.toString());
+				}
+				else
+					System.out.println("\n-> ID Não Encontrado!\n");
+
+
+				break;
+			}
+
+			// LISTAR CATEGORIA
+			case 10: {
+				Archive<Category> categoryFile;
+				Archive<Game> gameFile;
+
+				categoryFile = new Archive<>(Category.class.getConstructor(), "./db/category.db", "./db/index_category.db");
+				gameFile = new Archive<>(Game.class.getConstructor(), "./db/game.db", "./db/index.db");
+		
+				for (Category category: categoryFile.list()) {
+					System.out.print(category.toString());		
+					System.out.println("Jogos: ");
+					for(Game game: gameFile.list()){
+						if (game.categoryID == category.id)
+							System.out.println(game.name);	
+					}
+					System.out.println("");								
+				}
+
+				break;
+			}
+
+			
+			// MOSTRAR METADADOS DO DB
+			case 11: {
 				Archive<Game> gameFile;
 				gameFile = new Archive<>(Game.class.getConstructor(), "./db/game.db", "./db/index.db");
 
@@ -96,7 +222,7 @@ public class Main {
 			}
 
 			// FECHAR SISTEMA
-			case 7: {
+			case 12: {
 			    System.exit(0);
 			}
 			default:{
@@ -114,7 +240,7 @@ public class Main {
 			
 			try {
 				op = Integer.parseInt(in.nextLine());
-				if(op >= 1 && op <= 7) {
+				if(op >= 1 && op <= UserInterface.NUM_OPS) {
 					try {
 						chooseOp(op);
 					}
